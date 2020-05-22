f3eff69793965759f4ec26df0fcf2180a2e7777e
JoHannah Joy <jojojoy2012@gmail.com>
2020-05-21 23:05:30 -0700

    work on template kinks

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from .views import PantryItemViewSet # UserRegisterViewSet
 from rest_framework.routers import DefaultRouter
 
 
+# router = routers.DefaultRouter()
 router = DefaultRouter()
-# router.register('', UserRegisterViewSet, basename='users')
 router.register('', PantryItemViewSet, basename='pantry-items')
 # router.register('User', UserViewSet, basename='users')
-# router.register('PantryItem', PantryItemViewSet, basename='pantry-items')
+# router.register('PantryItem', views.PantryItemViewSet, basename='pantry-items')
 
 urlpatterns = router.urls
