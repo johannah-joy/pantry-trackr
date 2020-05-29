71d44f6c0bf9a97cd8bdcbf09ac89971ed55d2c1
JoHannah Joy <jojojoy2012@gmail.com>
2020-05-29 03:07:04 -0700

    still working on POST redirect

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,17 @@
+from django import forms
+from pantry_trackr.models import PantryItem
+
+class PantryItemForm(forms.ModelForm):
+    class Meta:
+        model = PantryItem
+        # fields = ['item_name', 'quantity_min', 'quantity_now']
+        fields = "__all__"
+
+
+
 # from django import forms
-# from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-# from django.contrib.auth.models import User
-# from pantry_trackr.models import *   # import user
-'''
-class UserCreationForm(forms.ModelForm):
-    name = forms.CharField(max_length=50)
-    email = forms.EmailField(max_length=254)
-    password = forms.CharField(widget=forms.PasswordInput())
-'''
 
-# class UserRegisterForm(UserCreationForm):
-
-#     class Meta:
-#         model = User
-#         fields = ["username", "password1", "password2"]
+# class PantryForm(forms.Form):
+#     item_name = forms.CharField(max_length=30)
+#     quantity_min = forms.IntegerField()
+#     quantity_now = forms.IntegerField()
--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 # from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from pantry_trackr.models import PantryItem
+from .forms import PantryItemForm
+from django.views.generic.edit import FormView
 from django.contrib.auth import logout
 
 # Create your views here.
@@ -23,32 +26,67 @@ class LoginView(generic.DetailView):
     success_url = reverse_lazy('home')
 
 
-class AddItemView(generic.CreateView):
-    model = PantryItem
-    # template_name = 'add-item.html'
-    template_name = 'my-pantry.html'
-    success_url = reverse_lazy('my-pantry')
+
+# class AddItemView(generic.FormView):
+#     form_class = PantryForm
+#     template_name = 'add-item.html'
+#     success_url = reverse_lazy('my-pantry')
+
+# class AddItemView(generic.CreateView):
+#     # model = PantryItem
+#     form_class = PantryItem
+#     # template_name = 'add-item.html'
+#     template_name = 'my-pantry.html'
+#     # success_url = reverse_lazy('my-pantry')
 
 #     # def
-
 #         # return
 
 
 # def addItem(request):
-#     return HttpResponseRedirect('/my-pantry/')
+#     pantry = PantryItem.objects.all()
+#     # item_name = request.POST['item_name']
+#     # quantity_min = request.POST['quantity_min']
+#     # quantity_now = request.POST['quantity_now']
+#     # return HttpResponseRedirect(reverse('users:my-pantry'))
+#     return render(request, 'add-item.html', {'pantry': pantry})
+
+# def addItem(request):
+#     form = PantryItemForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form.PantryItemForm()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'add-item.html', context)
+
+def addItem(request): 
+    if request.method == 'POST':
+        form = PantryItemForm(request.POST or None) 
+        if form.is_valid(): 
+            form.save()
+            pantry = PantryItem.objects.all()
+            return render(request, 'my-pantry.html', {'pantry': pantry})
+    else:
+        form_class = PantryItemForm
+    return render(request, "add-item.html", {
+        'form': form_class,})
 
 
-class MyPantryView(generic.CreateView):
-    model = PantryItem
-    template_name = 'my-pantry.html'
-    fields = ['item_now', 'quantity_min', 'quantity_now']
-    success_url = reverse_lazy('my-pantry')
 
-    # # ADD ITEM and USED AN ITEM will be in this view also
+# class MyPantryView(generic.CreateView):
+#     model = PantryItem
+#     template_name = 'my-pantry.html'
+#     # fields = ['item_now', 'quantity_min', 'quantity_now']
+#     success_url = reverse_lazy('my-pantry')
 
     # def
-
         # return
+
+def myPantry(request):
+    PantryItem.objects.create(item_name=request.POST['item_name'], quantity_min=request.POST['quantity_min'], quantity_now=request.POST['quantity_now'])
+    return HttpResponseRedirect(reverse('users:my-pantry'))
 
 
 class UsedItemView(generic.UpdateView):
@@ -58,7 +96,6 @@ class UsedItemView(generic.UpdateView):
     success_url = reverse_lazy('my-pantry')
 
 #     # def
-
 #         # return
 
 
@@ -68,7 +105,6 @@ class UsedItemView(generic.UpdateView):
 #     # fields or success_url =
 
 #     # def
-
 #         # return
 
 
